09811b3bb37bf8cc23b7140537b468b595291c34
Girlc7442 <girlc7442@wrdsb.ca>
2020-10-29 15:43:39 +0000

    Finished with comments

--- Main.java
+++ Main.java
@@ -5,7 +5,13 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
-  
+  JPanel mainPanel;
+
+  JTextField inputName;
+
+  JLabel helloName;
+
+  JButton sayHello;
 
 
   // Method to assemble our GUI
@@ -18,9 +24,41 @@ public class Main implements Runnable, ActionListener{
     frame.setSize(800,600);
     // shows the window
     frame.setVisible(true);
- 
-    
 
+    //initialize the mainPanel
+    mainPanel = new JPanel();
+    //disable any layout helpers
+    mainPanel.setLayout(null);
+
+    //initialize the text JTextField
+    inputName = new JTextField();
+    //set location and bounds
+    inputName.setBounds(200,50,400,30);
+
+    //initialize the JLabel
+    helloName = new JLabel();
+    //set location and bounds
+    helloName.setBounds(200,160,400,30);
+
+    //initialize the JButton
+    sayHello = new JButton("Say Hello");
+    //set location and bounds
+    sayHello.setBounds(350,230,100,30);
+    //set the action command on the buttons
+    sayHello.addActionListener(this);
+
+    //add the button to the mainPanel
+    mainPanel.add(sayHello);
+
+    //add the label to the mainPanel
+    mainPanel.add(helloName);
+
+    //add the text fields to the panel
+    mainPanel.add(inputName);
+    
+    //add the panel to the window
+    frame.add(mainPanel);
+ 
   }
 
   // method called when a button is pressed
@@ -28,6 +66,13 @@ public class Main implements Runnable, ActionListener{
     // get the command from the action
     String command = e.getActionCommand();
 
+    if(command.equals("Say Hello")){
+      //get the name from the box
+      String name = inputName.getText();
+
+      //say hello to the user
+      helloName.setText("Hello " + name);
+    }
   }
 
   // Main method to start our program
